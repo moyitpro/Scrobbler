f1e2a577c1b4ceb9519d46b3810d983c3d0435d5
chikorita157 <chikorita157@optonline.net>
2011-10-28 11:42:25 -0400

    Initial Commit

---
+++ Scrobbler/ScrobblerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ScrobblerAppDelegate.h
+//  Scrobbler
+//
+//  Created by Nanoha Takamachi on 10/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ScrobblerAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Scrobbler/ScrobblerAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  ScrobblerAppDelegate.m
+//  Scrobbler
+//
+//  Created by Nanoha Takamachi on 10/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ScrobblerAppDelegate.h"
+
+@implementation ScrobblerAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
